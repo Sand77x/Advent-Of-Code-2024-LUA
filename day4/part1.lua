4feb2ad399299703db44cbdfc2082a09312e32a3
Rendell Ngo <sand77x@gmail.com>
2024-12-11 20:12:16 +0800

    add better recursive solution for day 4 part 1

--- day4/part1.lua
+++ day4/part1.lua
@@ -1,5 +1,5 @@
 -- AOC 2024 Day 4 Part 1
--- done on 12/10/24
+-- done on 12/11/24
 
 local input = {}
 
@@ -12,93 +12,45 @@ for line in io.lines("input.txt") do
     table.insert(input, row)
 end
 
-local length = #input
 local total = 0
+local directions = { { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 } }
+local expectations = { X = "M", M = "A", A = "S", }
 
+-- recursively go in that direction expecting the next letter in the string
+local function go_in_direction(x, y, direction, last_letter)
+    local newx = x + direction[1]
+    local newy = y + direction[2]
+    local expected_letter = expectations[last_letter]
 
--- traverse row wise
-for i = 1, length do
-    local j = 1
-    while j <= length - 3 do
-        local c = input[i][j]
+    -- return 0 if out of bounds
+    if (newx < 1 or newx > #input) or (newy < 1 or newy > #input[1]) then
+        return 0
+    end
 
-        if c == 'X' then
-            if (input[i][j + 1] .. input[i][j + 2] .. input[i][j + 3]) == "MAS" then
-                total = total + 1
-            end
-        elseif c == 'S' then
-            if (input[i][j + 1] .. input[i][j + 2] .. input[i][j + 3]) == "AMX" then
-                total = total + 1
-            end
-        end
-        j = j + 1
+    -- if new char is expected letter
+    -- return 1 if expected letter is S (meaning XMAS is found)
+    -- else go deeper in that direction
+    if input[newx][newy] == expected_letter then
+        return expected_letter == "S" and 1 or go_in_direction(newx, newy, direction, expected_letter)
+    end
+
+    -- return 0 if new char is NOT expected letter
+    return 0
+end
+
+-- traverse from x, y into every direction
+local function traverse(x, y)
+    for i = 1, #directions do
+        total = total + go_in_direction(x, y, directions[i], "X")
     end
 end
 
--- traverse column wise
-for i = 1, length do
-    local j = 1
-    while j <= length - 3 do
-        local c = input[j][i]
-
-        if c == 'X' then
-            if (input[j + 1][i] .. input[j + 2][i] .. input[j + 3][i]) == "MAS" then
-                total = total + 1
-            end
-        elseif c == 'S' then
-            if (input[j + 1][i] .. input[j + 2][i] .. input[j + 3][i]) == "AMX" then
-                total = total + 1
-            end
+-- iterate through every char
+for i = 1, #input do
+    for j = 1, #input[i] do
+        if input[i][j] == "X" then
+            traverse(i, j)
         end
-        j = j + 1
-    end
-end
-
--- traverse / wise
-for i = 4, length * 2 - 4 do
-    local row = math.min(i, length)
-    local start_col = math.min(i - row + 1, length)
-    local col = start_col
-
-    while row > 3 and col <= length - 3 do
-        local c = input[row][col]
-
-        if c == 'X' then
-            if (input[row - 1][col + 1] .. input[row - 2][col + 2] .. input[row - 3][col + 3]) == "MAS" then
-                total = total + 1
-            end
-        elseif c == 'S' then
-            if (input[row - 1][col + 1] .. input[row - 2][col + 2] .. input[row - 3][col + 3]) == "AMX" then
-                total = total + 1
-            end
-        end
-
-        row = row - 1
-        col = col + 1
-    end
-end
-
--- traverse \ wise
-for i = 4, length * 2 - 4 do
-    local start_row = math.max(1, i - length + 1)
-    local row = start_row
-    local col = math.max(1, length - i + 1)
-
-    while row <= length - 3 and col <= length - 3 do
-        local c = input[row][col]
-
-        if c == 'X' then
-            if (input[row + 1][col + 1] .. input[row + 2][col + 2] .. input[row + 3][col + 3]) == "MAS" then
-                total = total + 1
-            end
-        elseif c == 'S' then
-            if (input[row + 1][col + 1] .. input[row + 2][col + 2] .. input[row + 3][col + 3]) == "AMX" then
-                total = total + 1
-            end
-        end
-
-        row = row + 1
-        col = col + 1
     end
 end
 
---
+++ day4/part1old.lua
@@ -0,0 +1,106 @@
+-- (OLD SOLUTION)
+-- AOC 2024 Day 4 Part 1
+-- done on 12/10/24
+
+local input = {}
+
+for line in io.lines("input.txt") do
+    local row = {}
+    for i = 1, #line do
+        row[i] = line:sub(i, i)
+    end
+
+    table.insert(input, row)
+end
+
+local length = #input
+local total = 0
+
+
+-- traverse row wise
+for i = 1, length do
+    local j = 1
+    while j <= length - 3 do
+        local c = input[i][j]
+
+        if c == 'X' then
+            if (input[i][j + 1] .. input[i][j + 2] .. input[i][j + 3]) == "MAS" then
+                total = total + 1
+            end
+        elseif c == 'S' then
+            if (input[i][j + 1] .. input[i][j + 2] .. input[i][j + 3]) == "AMX" then
+                total = total + 1
+            end
+        end
+        j = j + 1
+    end
+end
+
+-- traverse column wise
+for i = 1, length do
+    local j = 1
+    while j <= length - 3 do
+        local c = input[j][i]
+
+        if c == 'X' then
+            if (input[j + 1][i] .. input[j + 2][i] .. input[j + 3][i]) == "MAS" then
+                total = total + 1
+            end
+        elseif c == 'S' then
+            if (input[j + 1][i] .. input[j + 2][i] .. input[j + 3][i]) == "AMX" then
+                total = total + 1
+            end
+        end
+        j = j + 1
+    end
+end
+
+-- traverse / wise
+for i = 4, length * 2 - 4 do
+    local row = math.min(i, length)
+    local start_col = math.min(i - row + 1, length)
+    local col = start_col
+
+    while row > 3 and col <= length - 3 do
+        local c = input[row][col]
+
+        if c == 'X' then
+            if (input[row - 1][col + 1] .. input[row - 2][col + 2] .. input[row - 3][col + 3]) == "MAS" then
+                total = total + 1
+            end
+        elseif c == 'S' then
+            if (input[row - 1][col + 1] .. input[row - 2][col + 2] .. input[row - 3][col + 3]) == "AMX" then
+                total = total + 1
+            end
+        end
+
+        row = row - 1
+        col = col + 1
+    end
+end
+
+-- traverse \ wise
+for i = 4, length * 2 - 4 do
+    local start_row = math.max(1, i - length + 1)
+    local row = start_row
+    local col = math.max(1, length - i + 1)
+
+    while row <= length - 3 and col <= length - 3 do
+        local c = input[row][col]
+
+        if c == 'X' then
+            if (input[row + 1][col + 1] .. input[row + 2][col + 2] .. input[row + 3][col + 3]) == "MAS" then
+                total = total + 1
+            end
+        elseif c == 'S' then
+            if (input[row + 1][col + 1] .. input[row + 2][col + 2] .. input[row + 3][col + 3]) == "AMX" then
+                total = total + 1
+            end
+        end
+
+        row = row + 1
+        col = col + 1
+    end
+end
+
+print(total)
