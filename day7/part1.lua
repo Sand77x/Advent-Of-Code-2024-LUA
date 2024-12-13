96f4777dd9fec77547d56733b56a94525c9901cc
Rendell Ngo <sand77x@gmail.com>
2024-12-13 21:28:39 +0800

    day 7

---
+++ day7/part1.lua
@@ -0,0 +1,40 @@
+-- AOC 2024 Day 7 Part 1
+-- done on 12/12/24
+
+local i = 1
+local eqs = {}
+for line in io.lines("input.txt") do
+    local sep = line:find(":")
+    local res = line:sub(1, sep - 1)
+    eqs[i] = {
+        res = tonumber(res),
+        nums = {}
+    }
+    for num in line:sub(sep + 1, #line):gmatch("%d+") do
+        table.insert(eqs[i].nums, tonumber(num))
+    end
+    i = i + 1
+end
+
+local function traverse(res, nums, current_total, idx)
+    if current_total > res then return false end
+
+    if idx > #nums then
+        if current_total == res then
+            return true
+        else
+            return false
+        end
+    end
+
+    return traverse(res, nums, current_total + nums[idx], idx + 1) or
+        traverse(res, nums, current_total * nums[idx], idx + 1)
+end
+
+local ans = 0
+for _, eq in ipairs(eqs) do
+    local possible = traverse(eq.res, eq.nums, eq.nums[1], 2)
+    if possible then ans = ans + eq.res end
+end
+
+print(string.format("%.i", ans))
